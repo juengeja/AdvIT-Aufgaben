3640c70f62fde66304f2c9bf9a6c2cfc273174fa
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2021-10-29 11:05:17 +0200

    Aufgabe 15

--- src/Aufgabe15/TPRFileServer.java
+++ src/Aufgabe15/TPRFileServer.java
@@ -9,7 +9,6 @@ import java.net.InetAddress;
 public class TPRFileServer {
 
     public final int DEFAULT_PORT = 5999;
-    private final String FILEPATH = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "Files" + File.separator;
     int port;
 
     public static void main(String[] args) {
@@ -23,37 +22,13 @@ public class TPRFileServer {
     public TPRFileServer(int port) {
         this.port = port;
 
-        String reply = "";
-        TPRMyFile file = new TPRMyFile();
-
         try (DatagramSocket datagramSocket = new DatagramSocket(port)) {
 
             DatagramPacket data = new DatagramPacket(new byte[65507], 65507);
 
             while (true) {
                 datagramSocket.receive(data);
-                String input = new String(data.getData(), 0, data.getLength());
-                String[] message;
-
-                if (input.startsWith("READ ")) {
-
-                    input = input.substring(5);
-                    message = input.split(",");
-                    reply = file.readLine(FILEPATH, message[0], Integer.parseInt(message[1]));
-
-                } else if (input.startsWith("WRITE ")) {
-                    input = input.substring(6);
-                    message = input.split(",");
-                    file.writeLine(FILEPATH, message[0], Integer.parseInt(message[1]), message[2]);
-                    reply = "Written!";
-
-                } else {
-                    reply = "Not a correct input!";
-                }
-                InetAddress inetAddress = data.getAddress();
-                byte[] byteData = reply.getBytes();
-                datagramSocket.send(new DatagramPacket(byteData, byteData.length, inetAddress, data.getPort()));
-
+                new ThreadPerRequest(datagramSocket, data.getAddress(), data.getPort(), new String(data.getData(), 0, data.getLength()));
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/Aufgabe15/ThreadPerRequest.java
+++ src/Aufgabe15/ThreadPerRequest.java
@@ -1,14 +1,63 @@
 package Aufgabe15;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class ThreadPerRequest implements Runnable{
 
-    public ThreadPerRequest(){
+    DatagramSocket datagramSocket;
+    InetAddress inetAddress;
+    String request;
+    int port;
+    private final String FILEPATH = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "Files" + File.separator;
 
+    public ThreadPerRequest(DatagramSocket datagramsocket, InetAddress inetAddress, int port, String request){
+
+        this.datagramSocket = datagramsocket;
+        this.request = request;
+        this.inetAddress = inetAddress;
+        this.port = port;
+
+        this.run();
+
+        Thread.currentThread().interrupt();
+        return;
     }
 
     @Override
     public void run() {
 
+        String reply = "";
+        String[] message;
+        TPRMyFile file = new TPRMyFile();
+
+        if (request.startsWith("READ ")) {
+
+            request = request.substring(5);
+            message = request.split(",");
+            reply = file.readLine(FILEPATH, message[0], Integer.parseInt(message[1]));
+
+        } else if (request.startsWith("WRITE ")) {
+            request = request.substring(6);
+            message = request.split(",");
+            file.writeLine(FILEPATH, message[0], Integer.parseInt(message[1]), message[2]);
+            reply = "Written!";
+
+        } else {
+            reply = "Not a correct input!";
+        }
+
+        DatagramPacket data = new DatagramPacket(new byte[65507], 65507);
+        byte[] byteData = reply.getBytes();
+
+        try {
+            datagramSocket.send(new DatagramPacket(byteData, byteData.length, inetAddress, port));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
